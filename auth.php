0162fa36faacc76c5f6765ffa4708ceee7081695
zahra heydari <zheydari5701@gmail.com>
2022-08-02 09:25:26 +0430

    fghjfh

--- auth.php
+++ auth.php
@@ -2,7 +2,19 @@
 
 class auth
 {
-    public static function register() {
-        return 'man ro bezan';
-    }
+    Route::prefix('/v1')->group(function () {
+    Route::post('/login', [App\Http\Controllers\v1\AuthController::class, 'login']);
+    Route::post('/register', [App\Http\Controllers\v1\AuthController::class, 'register']);
+    Route::middleware(['token'])->group(function () {
+        Route::delete('/logout', [App\Http\Controllers\v1\AuthController::class, 'logout']);
+        Route::post('/refresh', [App\Http\Controllers\v1\AuthController::class, 'refresh']);
+
+        Route::prefix('/users')->group(function () {
+            Route::get('/', [App\Http\Controllers\v1\UserController::class, 'get_profile']);
+            Route::post('/', [App\Http\Controllers\v1\UserController::class, 'update_profile']);
+            Route::put('/change-pass', [App\Http\Controllers\v1\UserController::class, 'change_password']);
+
+            Route::get('/tokens', [App\Http\Controllers\v1\UserTokensController::class, 'user_tokens']);
+        });
+    });
 }
